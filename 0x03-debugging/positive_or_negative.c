081caeb7837fe63e54e82b364568e4e2474886a9
Emmanuel Oyibo <emminexworld@gmail.com>
2022-09-16 00:17:14 +0000

    First commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,9 +6,8 @@
  *
  * Return: 0 (Success)
  */
-int main(void)
+void positive_or_negative(int n)
 {
-	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -22,5 +21,4 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
 }
